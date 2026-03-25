54d731d8ecd59ffe34a7ddbf9c320545cba0b979
Cathy <cathy@MacBook-Pro-3.local>
2026-03-25 19:07:57 -0400

    Remove day-by-day schedule section and related components

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 import AboutPreview from "@/components/AboutPreview";
 import GalleryPreview from "@/components/GalleryPreview";
 import ClassesPreview from "@/components/ClassesPreview";
-import WeeklyScheduleImages from "@/components/WeeklyScheduleImages";
-import ScheduleSection from "@/components/ScheduleSection";
 import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
@@ -13,8 +11,6 @@ export default function Home() {
       <AboutPreview />
       <GalleryPreview />
       <ClassesPreview />
-      <WeeklyScheduleImages />
-      <ScheduleSection />
       <ContactSection />
     </>
   );
